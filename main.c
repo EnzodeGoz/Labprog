2f033b77767e84554aca9e489ec90b1b583ca959
EnzoGoz <enzogoz02@gmail.com>
2022-04-25 18:17:47 -0300

    renomear

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int addestoque(){
+
+    return 0;
+}
+
+int delestoque(){
+    
+    return 0;
+}
+
+int estoque(){
+    
+    return 0;
+}
+
+int exibirdados(){
+    
+    return 0;
+}
+
+int calculos(){
+    
+    return 0;
+}
+
+int main() {
+printf("Olá! Bem vindo ao ao programa de gerenciamento de compras.\n");
+printf("Escolha uma das opções no menu abaixo:\n");
+printf("1-Estoque\n2-Exibir a vitrine\n3-Carrinho de compras\n");
+return 0;
+}
